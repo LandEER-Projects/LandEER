7a9af140820357b3aefa8cb8c3665f3038fec70e
LandEER-Projects <equipe.landeer@gmail.com>
2026-06-27 02:29:36 -0300

    Change Instagram link to Google
    Updated Instagram link to point to Google.

--- src/data.js
+++ src/data.js
@@ -26,7 +26,7 @@ import { WhatsAppIcon } from './icons/WhatsAppIcon.jsx';
 
 export const socialLinks = [
   { label: 'E-mail', href: 'mailto:equipe.landeer@gmail.com', icon: Mail },
-  { label: 'Instagram', href: 'https://www.instagram.com/landeer_ofc/', icon: Instagram }, 
+  { label: 'Instagram', href: 'https://google.com', icon: Instagram }, 
   { label: 'Facebook', href: 'https://www.facebook.com/profile.php?id=61591624732442', icon: Facebook },
   { label: 'WhatsApp', href: 'https://wa.me/5521920054352', icon: WhatsAppIcon },
   { label: 'GitHub', href: 'https://github.com/LandEER-Projects', icon: Github },
